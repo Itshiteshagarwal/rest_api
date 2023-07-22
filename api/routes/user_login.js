483f49a404e0b6095b0a4d48bcf05984abdfcaf2
tutorials-git <agarwalhitesh551@gmail.com>
2023-07-22 19:44:50 +0530

    commited

--- api/routes/user_login.js
+++ api/routes/user_login.js
@@ -5,47 +5,46 @@ const bcrypt = require('bcrypt');
 const User = require('../model/user');
 const jwt = require('jsonwebtoken');
 
-
 router.use(express.json());
-router.post('/login', (req, res, next)=>{
-    User.find({username:req.body.username})
-    .exec()
-    .then(user=>{
-        if(user.length<1){
-            return res.status(401).json({
-                msg:'user not found'
-            })
-        }
-        bcrypt.compare(req.body.password,user[0].password,(err,result)=>{
-            if(!result){
-                return res.status(401).json({
-                    msg:'password not match'
-                })
+
+router.post('/login', (req, res, next) => {
+    User.findOne({ username: req.body.username })
+        .exec()
+        .then(user => {
+            if (!user) {
+                // User not found or invalid username
+                return res.status(401).json({ msg: 'Invalid credentials' });
             }
-            if(result){
-                const token = jwt.sign({
-                    username:user[0].username,
-                    email:user[0].email,
-                },
-                'this is dummy text',
-                {
-                    expiresIn:"24h"
+
+            bcrypt.compare(req.body.password, user.password, (err, result) => {
+                if (!result) {
+                    // Password doesn't match
+                    return res.status(401).json({ msg: 'Invalid credentials' });
                 }
-                );
-                res.status(200).json({
-                    username:user[0].username,
-                    email:user[0].email,
-                    token:token
-                })
-            }
+
+                if (result) {
+                    // Password matches, generate and send the JWT token
+                    const token = jwt.sign(
+                        {
+                            username: user.username,
+                            email: user.email,
+                        },
+                        'this is dummy text',
+                        { expiresIn: '24h' }
+                    );
+
+                    res.status(200).json({
+                        username: user.username,
+                        email: user.email,
+                        token: token
+                    });
+                }
+            });
         })
-    })
-    .catch(err=>{
-        console.log(err);
-        res.status(500).json({
-            error:err
-        })
-    })
-})
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: 'An error occurred' });
+        });
+});
 
 module.exports = router;
