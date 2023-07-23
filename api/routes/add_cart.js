b2e3068be7cef588a3f6b7030d620ea1f7405a3c
tutorials-git <agarwalhitesh551@gmail.com>
2023-07-23 21:20:18 +0530

    update

--- api/routes/add_cart.js
+++ api/routes/add_cart.js
@@ -2,20 +2,26 @@ const express = require('express');
 const router = express.Router();
 const Cart = require('../model/add_cart'); // Replace 'cart' with the appropriate model for your cart table
 
+// Set appropriate headers to allow frontend access
 router.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.header('Access-Control-Allow-Methods', '*'); // Allow all methods (GET, POST, PUT, DELETE, etc.)
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Specify the allowed methods for CORS
   next();
 });
+
 // Add a product to the cart
 router.post('/api/add_to_cart', async (req, res) => {
   try {
     const { productId, productName, productPrice, quantity } = req.body;
 
-    // Find the cart or create a new cart if it doesn't exist
+    // You might need to change this logic based on your use case.
+    // If you have a user authentication system, you should associate the cart with the user.
+    // For now, we are finding the cart without any filters, which will give you the first cart in the collection.
     let cart = await Cart.findOne();
+
     if (!cart) {
+      // If no cart is found, create a new cart with an empty products array.
       cart = new Cart({ products: [] });
     }
 
@@ -32,6 +38,7 @@ router.post('/api/add_to_cart', async (req, res) => {
 
     // Save the updated cart to the database
     const updatedCart = await cart.save();
+
     res.status(201).json({ message: 'Product added to cart', cart: updatedCart });
   } catch (err) {
     console.error(err);
