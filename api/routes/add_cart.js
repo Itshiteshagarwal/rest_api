ac3ab6d800b63ade439dac9e86191a9b90bed7bf
tutorials-git <agarwalhitesh551@gmail.com>
2023-07-23 19:40:22 +0530

    update

--- api/routes/add_cart.js
+++ api/routes/add_cart.js
@@ -3,23 +3,22 @@ const router = express.Router();
 const Cart = require('../model/add_cart');
 const Product = require('../model/products');
 const User = require('../model/user');
-const mongoose = require('mongoose');
 
 // Add a product to the cart
 router.post('/api/cart', async (req, res) => {
   try {
-    const { username, productId, quantity } = req.body;
-
-    // Check if the product exists in the database
-    const product = await Product.findById(productId);
-    if (!product) {
-      return res.status(404).json({ error: 'Product not found' });
-    }
+    const { username, productName, quantity } = req.body;
 
     // Find the user in the user table
     const user = await User.findOne({ username });
     if (!user) {
       return res.status(404).json({ error: 'User not found' });
+    }
+
+    // Check if the product exists in the database based on the product name
+    const product = await Product.findOne({ name: productName });
+    if (!product) {
+      return res.status(404).json({ error: 'Product not found' });
     }
 
     // Find the user's cart or create a new cart if it doesn't exist
@@ -30,21 +29,20 @@ router.post('/api/cart', async (req, res) => {
 
     // Check if the product is already in the cart
     const existingProductIndex = cart.products.findIndex(
-      (item) => item.productId.toString() === productId
+      (item) => item.productId.toString() === product._id.toString()
     );
     if (existingProductIndex !== -1) {
       return res.status(400).json({ error: 'Product already exists in the cart' });
     }
 
     // Add the product with the given quantity to the cart
-    cart.products.push({ productId: mongoose.Types.ObjectId(productId), quantity });
+    cart.products.push({ productId: product._id, quantity });
 
     // Save the updated cart to the database
     const updatedCart = await cart.save();
     res.status(201).json({ message: 'Product added to cart', cart: updatedCart });
   } catch (err) {
-    console.error(err); // Log the error to the console for debugging purposes
-    res.status(500).json({ error: 'Error adding product to cart' });
+    res.status(500).json({ error: 'Error adding product to cart', err });
   }
 });
 
