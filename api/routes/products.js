854187ad43d7a3c145e9b3dfaf7ca862e33d160f
tutorials-git <agarwalhitesh551@gmail.com>
2023-07-23 16:36:26 +0530

    push

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,6 +2,14 @@ const express = require('express');
 const router = express.Router();
 const Product = require('../model/products');
 
+// Middleware to handle CORS
+router.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 router.get('/api/products', async (req, res) => {
   try {
     const { categoryId } = req.query;
