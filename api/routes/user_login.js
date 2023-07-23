d0b47c14df8988de52e924a43f5791dcedea74bd
tutorials-git <agarwalhitesh551@gmail.com>
2023-07-23 14:31:14 +0530

    changes

--- api/routes/user_login.js
+++ api/routes/user_login.js
@@ -5,6 +5,17 @@ const bcrypt = require('bcrypt');
 const User = require('../model/user');
 const jwt = require('jsonwebtoken');
 
+// Enable CORS to allow requests from the frontend domain (replace 'your_frontend_domain' with the actual domain)
+router.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'your_frontend_domain');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'POST');
+    return res.status(200).json({});
+  }
+  next();
+});
+
 router.post('/login', (req, res, next) => {
   const { username, password } = req.body;
 
@@ -31,18 +42,13 @@ router.post('/login', (req, res, next) => {
             username: user.username,
             email: user.email,
           },
-          'your_secret_key_here',
+          'your_secret_key_here', // Replace 'your_secret_key_here' with your actual JWT secret key
           {
             expiresIn: '24h',
           }
         );
 
-        // Send success message and token back to the login page
         res.status(200).json({ msg: 'User login successful', token });
-
-        // Redirect the user to the home page (assuming the home page URL is '/home')
-        // Alternatively, you can use JavaScript to redirect on the client side after receiving the success response.
-        // res.redirect('/home');
       });
     })
     .catch(err => {
