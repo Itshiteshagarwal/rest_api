f7929674d85d6efdb887183b8be073c03fde189e
tutorials-git <agarwalhitesh551@gmail.com>
2023-07-24 13:14:57 +0530

    changes

--- api/routes/get_item.js
+++ api/routes/get_item.js
@@ -1,36 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const cors = require('cors');
 const Cart = require('../model/add_cart');
-
-// Set appropriate headers to allow frontend access
-router.use(cors({
-  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'], // Remove 'userid' from allowedHeaders
-}));
 
 // Middleware to handle user authentication
 const authenticateUser = (req, res, next) => {
-  const authorizationHeader = req.headers.authorization;
+  const { authorization } = req.headers;
 
-  if (!authorizationHeader) {
+  if (!authorization) {
     return res.status(401).json({ error: 'Unauthorized' });
   }
 
   // Assuming the authorization header value is the username
-  const username = authorizationHeader;
+  const username = authorization;
 
   // Attach the authenticated username to the request object for later use
   req.username = username;
   next();
 };
 
-// Get the cart data
-router.get('/cart_items', authenticateUser, async (req, res) => {
+// Get the cart data for the provided username
+router.get('/get_cart_items', authenticateUser, async (req, res) => {
   const { username } = req;
 
   try {
     // Check if the cart exists for the user
-    const existingCart = await Cart.findOne({ userId: username });
+    const existingCart = await Cart.findOne({ username });
 
     if (existingCart) {
       res.json({ cart: existingCart });
