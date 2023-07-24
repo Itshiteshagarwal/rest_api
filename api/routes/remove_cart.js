c140b10065ac2ab59a99d81ae17ce7a77636bc82
tutorials-git <agarwalhitesh551@gmail.com>
2023-07-24 13:46:48 +0530

    update

--- api/routes/remove_cart.js
+++ api/routes/remove_cart.js
@@ -2,11 +2,35 @@ const express = require('express');
 const router = express.Router();
 const CartItem = require('../model/add_cart');
 
-router.delete('/:productId', (req, res) => {
-  const productId = req.params.productId;
 
-  // Find the cart item by productId and remove it
-  CartItem.findOneAndDelete({ productId })
+router.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Specify the allowed methods for CORS
+  next();
+});
+// Middleware to handle user authentication
+const authenticateUser = (req, res, next) => {
+  // Assuming the username is stored in the request object after authentication
+  const { username } = req;
+
+  // Perform user authentication here (e.g., check the username against the database or JWT)
+  // For simplicity, we will assume the user is authenticated by checking if the username exists.
+  if (!username) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  // Attach the authenticated username to the request object for later use
+  req.username = username;
+  next();
+};
+
+router.delete('/productId', authenticateUser, (req, res) => {
+  const productId = req.params.productId;
+  const username = req.username;
+
+  // Find the cart item by productId and the associated username and remove it
+  CartItem.findOneAndDelete({ productId, username })
     .then(removedItem => {
       if (!removedItem) {
         return res.status(404).json({ error: 'Item not found in cart' });
