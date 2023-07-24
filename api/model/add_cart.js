aa35e258200d4d79653cd928594e9ab6e1c0e060
tutorials-git <agarwalhitesh551@gmail.com>
2023-07-24 12:16:13 +0530

    changes

--- api/model/add_cart.js
+++ api/model/add_cart.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 
 const cartSchema = new mongoose.Schema({
+  userId: {
+    type:String,
+    required: true,
+    ref: 'User' // Use the correct model name ('User') to reference the User model.
+  },
   products: [
     {
       productId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'products' // Use the correct model name ('Product') to reference the Product model.
+        ref: 'Product' // Use the correct model name ('Product') to reference the Product model.
       },
       productName: {
         type: String,
