c719bb3d359bd71e00d7993028fe3e25fe6e4b32
tutorials-git <agarwalhitesh551@gmail.com>
2023-07-24 14:34:40 +0530

    update

--- api/routes/remove_cart.js
+++ api/routes/remove_cart.js
@@ -4,31 +4,15 @@ const CartItem = require('../model/add_cart');
 
 router.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, username'); // Include 'username' in the allowed headers
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Specify the allowed methods for CORS
   next();
 });
 
-// Middleware to verify user authentication based on username
-const authenticateUser = (req, res, next) => {
-  const { username } = req.headers;
-
-  if (!username) {
-    return res.status(401).json({ error: 'Unauthorized' });
-  }
-
-  // Here, you can perform user authentication by checking the username against your database or any other means.
-  // For simplicity, we will assume the user is authenticated if the username is provided.
-
-  // Attach the authenticated username to the request object for later use
-  req.username = username;
-  next();
-};
-
-// API endpoint to delete an item from the cart by product name for a specific authenticated user
-router.delete('/api/remove-item', authenticateUser, async (req, res) => {
+// API endpoint to delete an item from the cart by product name
+router.delete('/api/remove-item', async (req, res) => {
   const { productName } = req.body;
-  const username = req.username;
+  const { username } = req.headers; // Get the 'username' from the request headers
 
   try {
     // Find the cart item by productName and the associated username and remove it
