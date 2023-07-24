d543b6157cbbf86d72c8ffd3f8554fa638c1664c
tutorials-git <agarwalhitesh551@gmail.com>
2023-07-24 17:13:04 +0530

    update

--- api/routes/update_product.js
+++ api/routes/update_product.js
@@ -1,45 +1,42 @@
 const express = require('express');
 const router = express.Router();
-const CartItem = require('../model/add_cart');
+const Cart = require('../model/add_cart');
 
-// Enable CORS for all routes
+// Set appropriate headers to allow frontend access
 router.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, username'); // Include 'username' in the allowed headers
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Specify the allowed methods for CORS
   next();
 });
 
-// API endpoint to update the product quantity in the cart by product name
-router.put('/update_quantity', async (req, res) => {
+// API endpoint to update the product quantity by product name
+router.patch('/update_quantity_by_name', async (req, res) => {
   const { productName, quantity } = req.body;
   const { username } = req.headers; // Get the 'username' from the request headers
 
   try {
-    // Find the cart item by productName and the associated username and update the quantity
-    const updatedCartItem = await CartItem.findOneAndUpdate(
-      { username, 'products.productName': productName },
-      { $set: { 'products.$.quantity': quantity } },
-      { new: true }
-    );
+    // Find the cart item by productName and the associated username
+    const cartItem = await Cart.findOne({ username, 'products.productName': productName });
 
-    if (!updatedCartItem) {
+    if (!cartItem) {
       return res.status(404).json({ error: 'Item not found in cart' });
     }
 
-    // Recalculate the price based on the updated quantity
-    updatedCartItem.products.forEach((product) => {
+    // Update the product quantity
+    cartItem.products.forEach((product) => {
       if (product.productName === productName) {
-        product.productPrice = product.productPrice * quantity;
+        product.quantity = quantity;
+        // Update the product price based on the new quantity (if required)
+        // product.productPrice = product.productPrice * quantity;
       }
     });
 
-    await updatedCartItem.save();
-
-    res.json({ message: 'Product quantity updated successfully', updatedCartItem });
+    await cartItem.save();
+    res.json({ message: 'Quantity updated successfully', cart: cartItem });
   } catch (error) {
     console.error('Error while updating quantity:', error);
-    res.status(500).json({ error: 'Failed to update product quantity' });
+    res.status(500).json({ error: 'Failed to update quantity' });
   }
 });
 
