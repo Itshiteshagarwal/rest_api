423bd970314899c2bdd40c2231835fc261efe914
tutorials-git <agarwalhitesh551@gmail.com>
2023-07-24 11:53:31 +0530

    changes

--- api/model/user.js
+++ api/model/user.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
+const { v4: uuidv4 } = require('uuid'); // Import the uuid library to generate unique IDs
 
-const userschema = new mongoose.Schema({
-    _id:mongoose.Schema.Types.ObjectId,
-    username:String,
-    email:String,
-    password:String,
-})
+// Define the user schema
+const userSchema = new mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  userId: { type: String, required: true, unique: true, default: uuidv4 }, // Generate a unique ID using uuid
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true }
+});
 
-module.exports = mongoose.model('user', userschema);
+module.exports = mongoose.model('user', userSchema);
